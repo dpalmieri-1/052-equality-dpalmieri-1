b357f0c5fac523e4988e32ecafd10749e3053911
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2025-10-16 18:10:54 +0000

    Fixed Match

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-
+function priceCheck(price){
+    if (price == 1.50){
+        document.getElementById('result').innerHTML = `match`;
+    }
+}
+function strictPriceCheck(price){
+    if (price === 1.50){
+        document.getElementById('result').innerHTML = `match`;
+    }
+}
+document.getElementById('result').innerHTML = priceCheck(1.50)
